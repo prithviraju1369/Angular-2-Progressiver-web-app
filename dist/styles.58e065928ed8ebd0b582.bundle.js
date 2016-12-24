3853bd1d5036b2ad53c5c878120fbf0ac492743f
prithviraju1369 <prithviraju1369@gmail.com>
2016-12-25 00:29:55 +0530

    create list ui

--- dist/styles.58e065928ed8ebd0b582.bundle.js
+++ dist/styles.58e065928ed8ebd0b582.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([3,5],{1002:function(n,o,c){c(450),n.exports=c(449)},449:function(n,o){},450:function(n,o){}},[1002]);
+webpackJsonp([4,6],{1015:function(n,o,c){c(461),n.exports=c(460)},460:function(n,o){},461:function(n,o){}},[1015]);
 //# sourceMappingURL=styles.58e065928ed8ebd0b582.bundle.map
