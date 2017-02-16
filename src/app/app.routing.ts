d68c24622577a53d6dfd9dc434bd4932314fcfcd
prithviraju1369 <prithviraju1369@gmail.com>
2017-02-16 10:28:38 +0530

    css changes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule,PreloadAllModules  } from '@angular/router';
 
 // all route and high level routes with lazy loading
 // + denotes lazy loading for those modules
@@ -18,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
